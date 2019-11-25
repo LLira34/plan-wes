67dcbb2465c63dfd796cba17733839f7aa9a0875
LLira34 <luislira1029@gmail.com>
2019-11-24 21:29:25 -0600

    first commit

--- app/src/main/java/net/loviedo/planwes/activities/TensParameterActivity.java
+++ app/src/main/java/net/loviedo/planwes/activities/TensParameterActivity.java
@@ -9,22 +9,12 @@ import android.widget.Button;
 import net.loviedo.planwes.R;
 
 public class TensParameterActivity extends AppCompatActivity {
-    private Button btnExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.Theme_AppCompat_Light_Dialog); //this line i added
+        setTheme(R.style.DialogCustomTheme); //this line i added
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tens_parameter);
-
-        // Inflate
-        btnExit = findViewById(R.id.btn_exit);
-        btnExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.exit(0);
-            }
-        });
     }
 
 }//End class
