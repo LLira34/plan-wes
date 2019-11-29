407adfd71bb1850f6a8242810052e5b6a945b644
UlisesGarcia <ulisesgarcia060@gmail.com>
2019-11-29 04:17:14 -0600

    counter

--- app/src/main/java/net/loviedo/planwes/activities/BipolarParameterActivity.java
+++ app/src/main/java/net/loviedo/planwes/activities/BipolarParameterActivity.java
@@ -3,17 +3,104 @@ package net.loviedo.planwes.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import net.loviedo.planwes.R;
 
 public class BipolarParameterActivity extends AppCompatActivity {
+
+    Button maxPort, minPort, maxTra, minTra;
+    EditText parametersPort, parametersTra;
+
+    private int contadorPort=2500;
+    private int contadorTra=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.DialogCustomTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_bipolar_parameter);
+
+        maxPort = findViewById(R.id.btn_plus_fre_port);
+        maxTra = findViewById(R.id.btn_plus_fre_tra);
+        minPort = findViewById(R.id.btn_less_fre_port);
+        minTra = findViewById(R.id.btn_less_fre_tra);
+
+        parametersPort = findViewById(R.id.edt_parameters_port);
+        parametersTra = findViewById(R.id.edt_parameters_tra);
+
+
+        maxPort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorPort==2510){
+                    maxPort.setText("Max");
+                }else if(contadorPort<2510){
+                    maxPort.setText("+");
+                    minPort.setText("-");
+                    contadorPort++;
+                    parametersPort.setText(""+contadorPort);
+                }
+
+
+            }
+        });
+
+        minPort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorPort==2500){
+                    minPort.setText("Min");
+                }else if(contadorPort>2500){
+                    minPort.setText("-");
+                    maxPort.setText("+");
+                    contadorPort--;
+                    parametersPort.setText(""+contadorPort);
+                }
+
+            }
+        });
+
+        maxTra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorTra==10){
+                    maxTra.setText("Max");
+                }else if(contadorTra<10){
+                    maxTra.setText("+");
+                    minTra.setText("-");
+                    contadorTra++;
+                    parametersTra.setText(""+contadorTra);
+                }
+
+            }
+        });
+
+        minTra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorTra==0){
+                    minTra.setText("Min");
+                }else if(contadorTra>0){
+                    minTra.setText("-");
+                    maxTra.setText("+");
+                    contadorTra--;
+                    parametersTra.setText(""+contadorTra);
+                }
+
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
 
 }//End class
--- app/src/main/java/net/loviedo/planwes/activities/TensParameterActivity.java
+++ app/src/main/java/net/loviedo/planwes/activities/TensParameterActivity.java
@@ -3,16 +3,98 @@ package net.loviedo.planwes.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import net.loviedo.planwes.R;
 
 public class TensParameterActivity extends AppCompatActivity {
+    Button maxFre, minFre, maxLong, minLong;
+    EditText parametersFre, parametersLong;
+
+    private int contadorFre=0;
+    private int contadorLong=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.DialogCustomTheme); //this line i added
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_tens_parameter);
+
+        maxFre = findViewById(R.id.btn_plus_fre);
+        maxLong = findViewById(R.id.btn_plus_long);
+        minFre = findViewById(R.id.btn_less_fre);
+        minLong = findViewById(R.id.btn_less_long);
+
+        parametersFre = findViewById(R.id.edt_parameters_fre);
+        parametersLong = findViewById(R.id.edt_parameters_long);
+
+
+        maxFre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorFre==10){
+                    maxFre.setText("Max");
+                }else if(contadorFre<10){
+                    maxFre.setText("+");
+                    minFre.setText("-");
+                    contadorFre++;
+                    parametersFre.setText(""+contadorFre);
+                }
+
+            }
+        });
+
+        maxLong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorLong==10){
+                    maxLong.setText("Max");
+                }else if(contadorFre<10){
+                    maxLong.setText("+");
+                    minLong.setText("-");
+                    contadorLong++;
+                    parametersLong.setText(""+contadorLong);
+                }
+
+            }
+        });
+
+        minFre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorFre==0){
+                    minFre.setText("Min");
+                }else if(contadorFre>0){
+                    minFre.setText("-");
+                    maxFre.setText("+");
+                    contadorFre--;
+                    parametersFre.setText(""+contadorFre);
+                }
+
+            }
+        });
+
+        minLong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorLong==0){
+                    minLong.setText("Min");
+                }else if(contadorLong>0){
+                    minLong.setText("-");
+                    maxLong.setText("+");
+                    contadorLong--;
+                    parametersLong.setText(""+contadorLong);
+                }
+
+            }
+        });
+
+
+
+
+
     }
 
 }//End class
